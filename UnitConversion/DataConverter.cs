b82869de387f4f43536c769433ec9e67f3aead74
DESKTOP-TGFQBC8\Atul <atul.mishra@ticketmaster.co.uk>
2018-12-14 04:49:04 +0000

    Added Data Converter

---
+++ UnitConversion/DataConverter.cs
@@ -0,0 +1,25 @@
+﻿namespace UnitConversion
+{
+    using UnitConversion.Base;
+
+    /// <summary>
+    /// Converts between data units.
+    /// </summary>
+    public class DataConverter : BaseUnitConverter
+    {
+        UnitFactors units = new UnitFactors("mb")
+        {
+            { new UnitFactorSynonyms("mb" , "megabyte"), 1 },
+            { new UnitFactorSynonyms("kb" , "kilobyte"), 1024 },
+        };
+
+        public DataConverter(string leftUnit, string rightUnit)
+        {
+            Instantiate(units, leftUnit, rightUnit);
+        }
+        public DataConverter()
+        {
+            Instantiate(units);
+        }
+    }
+}
---
+++ UnitConversionTests/DataConverterTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using UnitConversion;
+using UnitConversion.Base;
+
+namespace UnitConversionTests
+{
+    [TestClass()]
+    public class DataConverterTests
+    {
+        DataConverter converter = null;
+
+        [TestCleanup()]
+        public void Cleanup()
+        {
+            converter = null;
+        }
+
+        [TestMethod()]
+        public void mb_kb()
+        {
+            converter = new DataConverter("mb", "kb");
+            double valL = 1;
+            double valR = 1024;
+
+            Assert.AreEqual(valR, converter.LeftToRight(valL));
+            Assert.AreEqual(valL, converter.RightToLeft(valR));
+        }
+    }
+}
